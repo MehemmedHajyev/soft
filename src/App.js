c117aab9e27057f42a73d453c8e5ddf85d6c0731
MehemmedHajyev <hajyeffmehemmed8@gmail.com>
2023-08-21 17:22:55 +0400

    commit30

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
-// import Layout from "./components/Layout/Layout";
-// import './assets/font-awesome/css/font-awesome.css';
-// import './assets/font-awesome/css/font-awesome.min.css'; 
-// import './assets/font-awesome/css/font-awesome.min.css'
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import './assets/Css/global.css';
-// import Routing from "./components/Layout/Routing/Routing";
-// import { BrowserRouter } from "react-router-dom";
-// import ScrollToTop from './components/Layout/Routing/ScrollTop';
+import Layout from "./components/Layout/Layout";
+import './assets/font-awesome/css/font-awesome.css';
+import './assets/font-awesome/css/font-awesome.min.css'; 
+import './assets/font-awesome/css/font-awesome.min.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './assets/Css/global.css';
+import Routing from "./components/Layout/Routing/Routing";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from './components/Layout/Routing/ScrollTop';
 import TemirPage from "./components/Pages/Temir/TemirPage";
 
 function App() {  
@@ -16,12 +16,12 @@ function App() {
   return (
     <>
      {/* <BrowserRouter>
-      <Layout> */}
-        {/* <ScrollToTop /> */}
-        {/* <Routing />  */}
-      <TemirPage />
-      {/* </Layout>
+      <Layout>
+        <ScrollToTop />
+        <Routing /> 
+      </Layout>
       </BrowserRouter>       */}
+      <TemirPage />
     </>
   );
 }
